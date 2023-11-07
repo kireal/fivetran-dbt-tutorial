7cdbf45b3f3cde024ff6c3c8362922b605828fd3
Kirill Chufarov <kirillchufarov@gmail.com>
2023-11-07 16:21:38 -0500

    Added model for tutorial

---
+++ skills_matrix/models/example/skills_group_average.sql
@@ -0,0 +1,25 @@
+
+/*
+This is simple example of dbt model to schedule in Fivetran, based on google sheet data.
+*/
+
+{{ config(materialized='table') }}
+
+
+with skills as (
+    select
+        skill_group,
+        round(avg(delivery), 2) as average_skill_value,
+        count(*) as cnt_skilled
+    from google_sheets_bronze.skills_matrix_raw
+    where delivery <> 'NA'
+    group by skill_group
+)
+
+select
+    skill_group,
+    average_skill_value,
+    cnt_skilled
+from skills
+order by cnt_skilled desc
+
